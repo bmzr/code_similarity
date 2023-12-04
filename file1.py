2de2ea599f334b9feb6e4e9ad911e4c85de08d4d
Bryce Moser <bryce.moser@wsu.edu>
2023-12-03 19:22:40 -0800

    Added function profile creation via NodeVisitor traversal

--- file1.py
+++ file1.py
@@ -3,13 +3,16 @@ def subtract(one, two, variable=True):
     print("hello")
     three = one + two
     four = 4
-    return three, four
+    list = [5, 6]
+    list_len = len(list)
+    return three, four, list_len
 
 def main():
     var1 = (int)(input("var 1: "))
     var2 = (int)(input("var 2: "))
-    sum = subtract(var1, var2)
+    #sum = subtract(var1, var2)
     print(sum)
 
 if __name__ == "__main__":
-    main()
+    #main()
+    pass
--- main.py
+++ main.py
@@ -2,10 +2,38 @@ import ast
 
 class functiondef_visitor(ast.NodeVisitor):
     def __init__(self):
-        self.functiondefs = []
+        self.function_profiles = []
     
+    # get information for each function defined in the AST
     def visit_FunctionDef(self, node):
-        self.functiondefs.append(node)
+        parameter_count = len(node.args.args)
+        var_declaration_count = 0
+        if_count = 0
+        for_loop_count = 0
+        while_loop_count = 0
+        function_call_count = 0
+        bin_op_count = 0
+
+        for stmt in node.body:
+            if isinstance(stmt, ast.Assign):
+                var_declaration_count += 1
+                if isinstance(stmt.value, ast.Call):
+                    function_call_count += 1
+                if isinstance(stmt.value, ast.BinOp):
+                    bin_op_count += 1
+            elif isinstance(stmt, ast.If):
+                if_count += 1
+            elif isinstance(stmt, ast.For):
+                for_loop_count += 1
+            elif isinstance(stmt, ast.While):
+                while_loop_count += 1
+            elif isinstance(stmt, ast.Expr) and isinstance(stmt.value, ast.Call):
+                function_call_count += 1
+            elif isinstance(stmt, ast.Expr) and isinstance(stmt.value, ast.BinOp):
+                bin_op_count += 1
+
+        function_profile = (node.name, parameter_count, var_declaration_count, if_count, for_loop_count, while_loop_count, function_call_count, bin_op_count)
+        self.function_profiles.append(function_profile)
         self.generic_visit(node)
 
 # Given a code file pointer, create an AST structure.
@@ -15,58 +43,11 @@ def code_to_ast(code_file, print_ast = False):
     code_segment = ''
     for line in code_lines:
         code_segment += line
+    print(code_segment)
     code_ast = ast.parse(code_segment)
     if print_ast:
         print(ast.dump(code_ast, indent = 4))
     return code_ast
-
-# # checks the number of outputs in a function.
-# def check_output_count(function):
-#     output_count = 0
-#     function_return = False
-#     return_node = ast.Return
-#     for attribute in function.body:
-#         if isinstance(attribute, ast.Return):
-#             function_return = True
-#             return_node = attribute
-#             break
-#     if function_return is False:
-#         return output_count
-#     if isinstance(return_node.value, ast.Tuple) or isinstance(return_node.value, ast.List):
-#         output_count = len(return_node.value.elts)
-#     else:
-#         output_count = 1
-#     return output_count
-
-def check_return_type(function):
-    return_node = ast.Return
-    function_returns = False
-    for attribute in function.body:
-        if isinstance(attribute, ast.Return):
-            return_node = attribute
-            function_returns = True
-            return return_node.value
-        
-    if function_returns is False:
-        return None
-    
-# Creates a profile list from a FunctionDef based on similarity scoring attributes
-# (todo: document how/what we determine similarity with)
-def create_profile(function):
-    # number of parameters
-    parameter_count = 0
-    for arg in function.args.args:
-        parameter_count += 1
-    # function return type
-    return_type = check_return_type(function)
-    print("function return type: ")
-    print(return_type)
-    # number of variable declarations
-    
-    # number of if statements
-    # number of for loops
-    # number of while loops
-    # number of function calls
 
 def compare_codes(list_1, list_2):
     for function_l1 in list_1:
@@ -86,15 +67,16 @@ def main():
     print("\n------------------CODE FILE 2------------------")
     ast_2 = code_to_ast(code_file_2, print_ast=True)
 
-    ast_node_visitor = functiondef_visitor()
-    ast_node_visitor.visit(ast_1)
-    function_list_1 = ast_node_visitor.functiondefs
-    ast_node_visitor.visit(ast_2)
-    function_list_2 = ast_node_visitor.functiondefs
-
-    create_profile(function_list_1[0])
-    create_profile(function_list_1[1])
-    compare_codes(function_list_1, function_list_2)
+    ast_1_node_visitor = functiondef_visitor()
+    ast_1_node_visitor.visit(ast_1)
+    function_list_1 = ast_1_node_visitor.function_profiles
+    ast_2_node_visitor = functiondef_visitor()
+    ast_2_node_visitor.visit(ast_2)
+    function_list_2 = ast_2_node_visitor.function_profiles
+    print("FUNCTION LIST 1: ", end = '')
+    print(function_list_1)
+    print("FUNCTION LIST 2: ", end = '')
+    print(function_list_2)
     print("End.")
 
 if __name__ == "__main__":
